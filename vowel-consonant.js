b041dc96ea3c214d35c6c569fef19c725a93c848
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-15 01:23:59 +0600

    vowel-consonat program

--- vowel-consonant.js
+++ vowel-consonant.js
@@ -10,6 +10,31 @@ if (
   letter === "u"
 ) {
   document.getElementById("program1").innerHTML = `Letter is vowel`;
-} else {
-  document.getElementById("program1").innerHTML = ` Letter is consonant`;
+} else if (letter === "b" ||
+    letter === "c" ||
+    letter === "d" || 
+    letter === "f" || 
+    letter ==="g" ||
+     letter === "h" || 
+     letter === "j" || 
+     letter === "k" || 
+     letter === "l" || 
+     letter === "m" || 
+     letter === "n" || 
+     letter === "p" || 
+     letter === "q" ||
+      letter === "r" || 
+      letter === "s" || 
+      letter === "t" ||
+       letter === "v" ||
+        letter === "w" ||
+         letter === "x" || 
+         letter === "y" || 
+         letter === "z"){
+    document.getElementById("program1").innerHTML = ` Letter is consonant`;
+
+
+}
+else {
+  document.getElementById("program1").innerHTML = `Not a letter`;
 }
