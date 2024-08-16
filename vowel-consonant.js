37ac80a91be8b2d8d1743894eecb379f6d303c4f
MD JAKARIA <158171159+joy094@users.noreply.github.com>
2024-08-16 22:29:39 +0600

    Update vowel-consonant.js

--- vowel-consonant.js
+++ vowel-consonant.js
@@ -3,6 +3,10 @@ ch = ch.toLowerCase();
 if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
   document.write(` '${ch}' is vowel <br>`);
 } 
-else {
+else if(ch> 'a'&& ch<='z') {
   document.write(` '${ch}' is consonant  <br>`);
+}
+else{
+
+  document.write('this not a letter')
 }
