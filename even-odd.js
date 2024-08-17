77af2c8f815b235c22797dda8b7035d0c84c7fc1
Mohammad ashraful Islam <ashraful90life@gmail.com>
2024-08-17 20:10:07 +0600

    even-odd

---
+++ even-odd.js
@@ -0,0 +1,13 @@
+//  even or odd using if , else
+
+const num = prompt("Enter a number: ");
+
+//check if the number is even
+if (num % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
