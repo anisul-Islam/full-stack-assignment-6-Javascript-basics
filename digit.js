8212a62d60b8fe457fe5fc6c06481c516449ddb1
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-09-02 12:03:36 +0600

    final commit

---
+++ digit.js
@@ -0,0 +1,31 @@
+let number = Number(prompt ("Enter a digital number"));
+if (number==0){
+    console.log ("Zero");
+}
+else if (number==1){
+    console.log ("One");
+}
+else if (number==2){
+    console.log ("Two");
+}
+else if (number==3){
+    console.log ("Three");
+}
+else if (number==4){
+    console.log ("Four");
+}
+else if (number==5){
+    console.log ("Five");
+}
+else if (number==6){
+    console.log ("Six");
+}
+else if (number==7){
+    console.log ("Seven");
+}
+else if (number==8){
+    console.log ("Eight");
+}
+else {
+    console.log ("Nine");
+}
---
+++ even-odd.js
@@ -0,0 +1,10 @@
+let number = Number(prompt("Enter any number :"));
+
+if (number===0) {
+    console.log ("The number is Neutral")
+} else if (number % 2 == 0) {
+    console.log (" The number is Even");
+}
+else {
+    console.log ("The num is Odd");
+}
