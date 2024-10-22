ddb2045fe8b3e438535982e6e2d6910b184d5c07
“noyonnabi78” <noyonnabi@gmail.com>
2024-10-22 06:21:22 +0600

    button color changed

--- vowel-consonant.js
+++ vowel-consonant.js
@@ -7,45 +7,19 @@ function vowelConsonent() {
     letter = letter.toLowerCase();
     
 
-    if(letter === 'a' ||
-        letter === 'e' ||
-        letter === 'i' ||
-        letter === 'o' ||
-        letter === 'u' 
-        )
-        {
-        document.getElementById("consonent-vowel").innerHTML = (`${letter} is a vowel`);
-        }
-        else if ( letter === 'b' ||
-            letter === 'c' ||
-            letter === 'd' ||
-            letter === 'f' ||
-            letter === 'g' ||
-            letter === 'h' ||
-            letter === 'j' ||
-            letter === 'k' ||
-            letter === 'l' ||
-            letter === 'm' ||
-            letter === 'n' ||
-            letter === 'p' ||
-            letter === 'q' ||
-            letter === 'r' ||
-            letter === 's' ||
-            letter === 't' ||
-            letter === 'v' ||
-            letter === 'w' ||
-            letter === 'x' ||
-            letter === 'y' ||
-            letter === 'z' 
-        
-        )
-        {
-            document.getElementById("consonent-vowel").innerHTML = (`${letter} is a consonent. `);
+    if (!/^[a-z]$/.test(letter)) {
+        console.log("invalid Input");
+        } else if (
+        letter === "a" ||
+        letter === "e" ||
+        letter === "i" ||
+        letter === "o" ||
+        letter === "u"
+        ) {
+        console.log("vowel");
         } else {
-            document.getElementById("consonent-vowel").innerHTML = (`${letter} is not an enlish letter.`);
-        };
- 
-
-
+        console.log("consonant");
+        } ;
 
 }
+vowelConsonent();
